dec04c0daf6e944941f246fb9287250213d7d3ce
The Rebo <95853374+TheRebo@users.noreply.github.com>
2024-01-14 12:40:04 +0700

    Menambahkan baris lisensi

--- pass-man-ind.py
+++ pass-man-ind.py
@@ -1,5 +1,18 @@
-# Smart Password Manager
-# A Python script that lets you store, view, and delete passwords for different services
+#    Smart Password Manager - A Python script that lets you store, view, and delete passwords for different services.
+#    Copyright (C) 2024 TheRebo
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # Import the modules we need
 import os # For file operations
