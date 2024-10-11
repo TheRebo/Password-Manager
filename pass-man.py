4798b9835eaad6dede321619337cfc04bf8ababd
TheRebo <naimannafisantosa@gmail.com>
2024-10-11 22:47:21 +0700

    Changing license to MIT

--- pass-man.py
+++ pass-man.py
@@ -1,18 +1,5 @@
-#    Smart Password Manager - A Python script that lets you store, view, and delete passwords for different services.
-#    Copyright (C) 2023-2024 TheRebo
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+## Copyright (c) 2023-2024 Na'im Annafi Santosa (TheRebo)
+## Licensed under the MIT License
 
 # ——— Import Module ———
 import os, time, sys, string, difflib
